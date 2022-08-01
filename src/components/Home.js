4ac9171daa1e0fc4ccf2d3fb8589780e20b23268
Tonny Oseko <tonnyoseko0@gmail.com>
2022-08-01 22:03:16 +0300

    created Home.js component

---
+++ src/components/Home.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import Items from "./Items";
+
+function Home() {
+  const [items, setItems] = useState([]);
+  const [searchItems, setSearchItems] = useState([]);
+  const url = "https://tonyoseko99.github.io/json-api/db.json";
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => {
+        setItems(response.products);
+      });
+  }, []);
+
+  useEffect(() => {
+    setSearchItems(items);
+  }, [items]);
+
+  return (
+    <div className="container">
+      <div className="input">
+        {/* search functionality */}
+        <input
+          id="search-input"
+          type="text"
+          placeholder="search..."
+          onChange={(e) => {
+            let query = items.filter((item) =>
+              item.title.toLowerCase().includes(e.target.value.toLowerCase())
+            );
+            console.log("search", query);
+            setSearchItems(query);
+          }}
+        />
+      </div>
+
+      <div className="card-container">
+        {searchItems.map((item) => {
+          return (
+            
+              <Items item={item} />
+            
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+export default Home;
