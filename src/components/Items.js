7f38e87ab098a63fc4b869a0eba87f32db8b7d04
Tonny Oseko <tonnyoseko0@gmail.com>
2022-08-01 22:09:41 +0300

    parse the item props from the homepage to the Items component

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,6 +1,20 @@
 import React from "react";
 
-function Item(){
-
+function Item({item}) {
+  return (
+    <>
+      <div className="card" key={item.id}>
+        <img src={item.image} alt="title"></img>
+        <h2>{item.title}</h2>
+        <p>{item.description}</p>
+        <div>
+          <h3>price: ${item.price}</h3>
+          <Link to={`item/${item.id}`} onClick={() => <Item key={item.id} />}>
+            Buy
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Item;
