54539650f9c54fa3afe1033e45ccaf3ed6cc125e
Tonny Oseko <tonnyoseko0@gmail.com>
2022-08-02 06:41:59 +0300

    style the login form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
@@ -6,15 +6,18 @@ import Footer from "./components/Footer";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Item from "./components/Item";
 import Items from "./components/Items";
+import Login from "./components/Login";
+
 function App() {
+
+  const [tokens, setTokens] = useState(localStorage.getItem("userToken") ?? null);
   return (
     <div className="root">
       <Navbar />
+      
       <Router>
-        
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/items" element={<Items />} />
+          <Route exact path="/" element={tokens ? <Home /> : <Login tokens={tokens} setTokens={setTokens} />} />
           <Route path="/item/id:" element={<Item />} />
           <Route>404 Error Page</Route>
         </Routes>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios'
 
-function Login(){
+function Login() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
+  userUrl = 'https://fakestoreapi.com/auth/login';
+  const userHandler = () => {
+    axios(
+        
+    )
+  }
+
+  return (
+    <div className="login">
+      <div className="login-form">
+        <h2>Login</h2>
+        <input
+          type="text"
+          placeholder="username"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <small>eror placeholder</small>
+        <button>Login</button>
+      </div>
+    </div>
+  );
 }
 export default Login;
