ad45247b6439aec93a5c7bbca2c3cf7bc1ce95a2
Tonny Oseko <tonnyoseko0@gmail.com>
2022-08-02 09:00:56 +0300

    add a background color to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
 import React from "react";
 
-function Navbar({setToken}){
-    const logoutHandler = () => {
-        setToken("");
-        localStorage.clear();
-    }
-    return(
-        <nav className="navbar">
-            <h1>Jenga Shop</h1>
-            <button onClick={() => logoutHandler()}>Logout</button>
-        </nav>
-    )
-
+function Navbar({ setToken }) {
+  const logoutHandler = () => {
+    setToken("");
+    localStorage.clear();
+  };
+  return (
+    <div className="main">
+      <nav className="navbar">
+        <h1>Jenga Shop</h1>
+        <button onClick={() => logoutHandler()}>Logout</button>
+      </nav>
+    </div>
+  );
 }
 export default Navbar;
